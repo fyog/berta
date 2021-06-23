678c413995472d5ea3982d413bb3a5e8324694ac
eross <eross@10.0.0.69>
2021-06-23 13:02:01 -0600

    working on time step methods

--- Mass.java
+++ Mass.java
@@ -1,8 +1,9 @@
 public class Mass {
-	
+		
+	// Fields
 	private double mass;
+	private double G;
 	private ThreeVector	position, velocity, acceleration;
-	final private double R_VALUE = 1.;
 	
 	// Constructors ---------------------------------------------------------------------------------------------------
 	
@@ -12,11 +13,12 @@ public class Mass {
 	 * @param mass
 	 * @param position
 	 */
-	public Mass(double mass, ThreeVector position) {
+	public Mass(double mass, ThreeVector position, double G) {
 		this.mass = mass;
 		this.position = position;
-		this.velocity = new ThreeVector(0, 0, 0);
-		this.acceleration = new ThreeVector(0, 0, 0);
+		this.velocity = new ThreeVector();
+		this.acceleration = new ThreeVector();
+		this.G = G;
 	}
 
 	// Getters and setters --------------------------------------------------------------------------------------------
@@ -111,73 +113,57 @@ public class Mass {
 	 */
 	public void predict(double deltaTime) {
 		
-		// Euler's method for velocity
+		// Newton's method for velocity
 		double x_vel = this.getVelocity().getX() + deltaTime * this.getAcceleration().getX();
 		double y_vel = this.getVelocity().getY() + deltaTime * this.getAcceleration().getY();
 		double z_vel = this.getVelocity().getZ() + deltaTime * this.getAcceleration().getZ();
-		
-		// Update velocity
+	
+		//  Updated velocity
 		ThreeVector newVelocity = new ThreeVector(x_vel, y_vel, z_vel);
-		this.setVelocity(newVelocity);
- 
-		// Euler's method for position
-		double x_pos = this.getPosition().getX() + deltaTime * newVelocity.getX();
+		this.setVelocity(newVelocity); 
+		
+		// Newton's method for position using new velocity
+		double x_pos = this.getPosition().getX() + deltaTime * newVelocity.getX(); 
 		double y_pos = this.getPosition().getY() + deltaTime * newVelocity.getY();
 		double z_pos = this.getPosition().getZ() + deltaTime * newVelocity.getZ();
 
-		// Update position
+		// Updated position
 		ThreeVector newPosition = new ThreeVector(x_pos, y_pos, z_pos);
 
 		this.setPosition(newPosition);
 	}
 	
 	/**
-	 * Calculates the acceleration due to the gravitational pull of the passed fixed point object. If the distance 
-	 * between the two objects is less than one unit then the gravity calculation is calculated as though the objects 
-	 * are only one unit apart. This avoids singularity.
+	 * Calculates the acceleration due to the gravitational pull of the passed fixed point object.
 	 * 
 	 * @param fixPoint
 	 * @return ThreeVector acceleration
 	 */
-	public ThreeVector gravity(FixedPoint fixPoint) {
+	public ThreeVector gravity(Mass mass) {
 		
-		// Big G constant
-		final double G = 6.67 * Math.pow(10, -3);
-		
-		double accelerationX, accelerationY, accelerationZ;
-		
-		// Currently, does not operate in the z-direction
-		accelerationZ = 0;
+		double accelerationX, accelerationY;
 		
 		// Determine acceleration due to gravity using Newton's Law of Universal Gravitation
-		// If x distance is between R_VALUE and -R_VALUE, then set the x component of acceleration to zero
-		double distanceX = this.getPosition().getX() - fixPoint.getPosition().getX();
-		if (distanceX < R_VALUE && distanceX > -R_VALUE) {
-			accelerationX = 0;
-		} else {
-			accelerationX = fixPoint.getMass() * G / (distanceX * distanceX);
-		}
+	
+		double distanceX = this.getPosition().getX() - mass.getPosition().getX();
+		accelerationX = mass.getMass() * G / (distanceX * distanceX);
+		double distanceY = this.getPosition().getY() - mass.getPosition().getY();
+		accelerationY = mass.getMass() * G / (distanceY * distanceY);
 		
-		// If y distance is between R_VALUE and -R_VALUE, then set the y component of acceleration to zero
-		double distanceY = this.getPosition().getY() - fixPoint.getPosition().getY();
-		if (distanceY < R_VALUE && distanceY > -R_VALUE) {
-			accelerationY = 0;
-		} else {
-			accelerationY = fixPoint.getMass() * G / (distanceY * distanceY);
-		}
 		
 		// Determine the correct direction for each acceleration component
-		if (distanceX > 0) {
-			accelerationX *= -1;
-		}
-		if (distanceY > 0) {
-			accelerationY *= -1;
-		}
+		if (distanceX > 0) accelerationX *= -1;
+		if (distanceY > 0) accelerationY *= -1;
+		
 		
 		// Create new vector
-		ThreeVector acceleration = new ThreeVector(accelerationX, accelerationY, accelerationZ);
+		ThreeVector acceleration = new ThreeVector(accelerationX, accelerationY, 0);
 		
 		// Return the acceleration
-		return acceleration;	
+		if ((distanceX < 1 && distanceX > -1) || (distanceY < 1 && distanceY > -1)) return new ThreeVector();
+		else return acceleration;	
 	}
+	/**
+	 * Calculates the acceleration due to the spring force on the passed mass.
+	 */
 }
--- Universe.java
+++ Universe.java
@@ -7,14 +7,13 @@ import javax.swing.JFrame;
 public class Universe extends Canvas {
 	
 	private Mass mass;
+	private Mass mass2;
 	private FixedPoint fixedPoint;
-
-	// Constructors ---------------------------------------------------------------------------------------------------------------------------------------------
 
 	/**
 	 * Constructor method.
 	 * 
-	 * @param mass
+	 * @param mass 
 	 * @param fixedPoint
 	 */
 	public Universe(Mass mass, FixedPoint fixedPoint) {
@@ -24,21 +23,29 @@ public class Universe extends Canvas {
 		this.fixedPoint = fixedPoint;
 	}
 	
-	// Overwritten methods --------------------------------------------------------------------------------------------------------------------------------------
+	public Universe (Mass mass, Mass mass2) {
+		this.setSize(1600, 1600);
+		this.setBackground(Color.BLACK);
+		this.mass = mass;
+		this.mass2 = mass2;
+	}
 	
 	@Override
+	/**
+	 * Paints the graphics.
+	 * 
+	 * @param g
+	 */
 	public void paint(Graphics g) {
 		
 		// Draw a circle to represent the fixed point
 		g.setColor(Color.RED);
-		g.fillOval((int) fixedPoint.getPosition().getX(), (int) fixedPoint.getPosition().getY(), 30, 30);
+		g.fillOval((int) mass2.getPosition().getX(), (int) mass2.getPosition().getY(), 30, 30);
 		
 		// Draw a circle to represent the mass
 		g.setColor(Color.GREEN);
 		g.fillOval((int) mass.getPosition().getX(), (int) mass.getPosition().getY(), 15, 15);
 	}
-	
-	// Main method -----------------------------------------------------------------------------------------------------------------------------------------------
 	
 	/**
 	 * Main method.
@@ -48,18 +55,20 @@ public class Universe extends Canvas {
 	public static void main(String[] args) {
 		
 		// Create system components
-		ThreeVector vec1 = new ThreeVector(700, 600, 0);
-		ThreeVector vec2 = new ThreeVector(800, 800, 0);
-		Mass mass = new Mass(5000, vec1);
-		FixedPoint fixPoint = new FixedPoint(500000, vec2);
+		ThreeVector vec1 = new ThreeVector(300, 300, 0);
+		ThreeVector vec2 = new ThreeVector(400, 400, 0);
+		Mass mass = new Mass(.05, vec1, 1);
+		Mass mass2 = new Mass(50, vec2, 1);
 		
 		// Set mass' initial velocity and initial acceleration
-		mass.setVelocity(new ThreeVector(0, 0, 0));
+		mass.setVelocity(new ThreeVector(1, 0, 0));
 		mass.setAcceleration(new ThreeVector(0, 0, 0));
+		mass2.setVelocity(new ThreeVector(0, 0, 0));
+		mass2.setAcceleration(new ThreeVector(0, 0, 0));
 		
 		// Create window
 		JFrame frame = new JFrame("Eric's Universe");
-		Universe universe = new Universe(mass, fixPoint);
+		Universe universe = new Universe(mass, mass2);
         frame.add(universe);
         frame.pack();
         frame.setVisible(true);
@@ -75,16 +84,18 @@ public class Universe extends Canvas {
 		while (running) {
 			
 			// Find and update mass acceleration due to gravity
-			ThreeVector accel = mass.gravity(fixPoint);
+			ThreeVector accel = mass.gravity(mass2);
+			ThreeVector accel2 = mass2.gravity(mass);
 			
 			// Set acceleration of mass
 			mass.setAcceleration(accel);
+			mass2.setAcceleration(accel2);
 			 
 			// Predict new position and velocity based off of new acceleration
 			mass.predict(deltaTime);
-			
+			mass2.predict(deltaTime);
 			// Repaint the canvas every ten 10 time steps
-			if ( (int) time % 1 == 0) {
+			if ( (int) time % 10 == 0) {
 				universe.repaint();
 			}
 			
