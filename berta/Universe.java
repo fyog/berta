660d940626fd0a15aee1c5e8e8db1ab867e918ac
eross <eross@10.0.0.69>
2021-06-25 15:18:35 -0600

    loooking good

--- berta/Universe.java
+++ berta/Universe.java
@@ -2,14 +2,13 @@ package berta;
 
 import java.awt.Canvas;
 import java.awt.Color;
-import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.util.Random;
-
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,27 +18,42 @@ public class Universe extends Canvas implements ActionListener {
 	
 	// Fields
 	private static final long serialVersionUID = 1L; // ?
+	private static final int SIM_WINDOW_WIDTH = 800;
+	private static final int SIM_WINDOW_LENGTH = 800;
 	Mass[] masses;
 	boolean paused = false;
+	Random rand = new Random();
+
+	
+	
 	/**
 	 * Constructor method.
 	 * 
-	 * @param mass 
-	 * @param fixedPoint
+	 * @param Mass[] masses
 	 */
 	
 	public Universe (Mass[] masses) {
 		this.masses = masses;
 	}
 	
+	/*
+	 * Setter for the paused field.
+	 * 
+	 * @param boolean value for the paused field
+	 */
 	public void setPaused(boolean value) {
 		this.paused = value;
 	}
 	
+	/*
+	 * Getter for the paused field.
+	 * 
+	 * @returns boolean paused
+	 */
 	public boolean getPaused() {
 		return paused;
 	}
-	
+			
 	@Override
 	/**
 	 * Paints the graphics.
@@ -47,24 +61,24 @@ public class Universe extends Canvas implements ActionListener {
 	 * @param g
 	 */
 	public void paint(Graphics g) {
-	
+		
 		for (Mass m : masses) {
 			
 			g.setColor(m.getColor());
 			g.fillOval((int) m.getPosition().getX(), 
-				(int) m.getPosition().getY(), 30, 30);
+				(int) m.getPosition().getY(), (int) m.getPosition().getZ()/10, (int) m.getPosition().getZ()/10);
 		}
+		
 		if (masses.length == 2) {
 			Mass mass = masses[0];
 			Mass mass2 = masses[1];
-			
-			g.setColor(Color.BLUE);
-			
+						
 			// Draw a line connecting the centers of mass
-			g.drawLine((int) mass.getPosition().getX() + 15, 
-					(int) mass.getPosition().getY() + 15, 
-				(int) mass2.getPosition().getX() + 15, 
-				(int) mass2.getPosition().getY() + 15);
+			g.setColor(Color.BLUE);
+			g.drawLine((int) mass.getPosition().getX() + (int) mass.getPosition().getZ()/20, 
+				(int) mass.getPosition().getY() + (int) mass.getPosition().getZ()/20, 
+				(int) mass2.getPosition().getX() + (int) mass2.getPosition().getZ()/20, 
+				(int) mass2.getPosition().getY() + (int) mass2.getPosition().getZ()/20);
 		}
 	}
 	
@@ -72,16 +86,48 @@ public class Universe extends Canvas implements ActionListener {
 		
 		// Create window
 		JFrame frame = new JFrame("Eric's Universe");
-		//JPanel panel = new JPanel(new FlowLayout());
-		Universe universe = new Universe(masses);
-		universe.setSize(800, 800);
-		universe.setBackground(Color.BLACK);
-		//JButton button = new JButton("Pause");
+		JPanel panel = new JPanel(new FlowLayout());
+		this.setSize(SIM_WINDOW_WIDTH, SIM_WINDOW_LENGTH);
+		this.setBackground(Color.BLACK);
 		
-		//button.addActionListener(this);
-		//panel.add(universe);
-		//panel.add(button);
-		frame.add(universe);
+		JButton pause_button = new JButton("Pause");
+		JButton reset_button = new JButton("Reset");
+		
+		pause_button.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+		    	if (paused) {
+					paused = false;
+					System.out.println("Unpaused");
+				} else {
+					paused = true;
+					System.out.println("Pause");
+				}
+		    }
+		});
+		
+		reset_button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				paused = false;
+				// Recreate system components
+				ThreeVector pos1 = new ThreeVector(500, 500, 500);
+				ThreeVector pos2 = new ThreeVector(400, 400, 100);
+				ThreeVector vel1 = new ThreeVector(-5, 0, 0);
+				ThreeVector vel2 = new ThreeVector(5, 0, 0);
+				masses[0].setPosition(pos1);
+				masses[0].setVelocity(vel1);
+				masses[1].setPosition(pos2);
+				masses[1].setVelocity(vel2);
+				
+			}
+		});
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		panel.setPreferredSize(new Dimension(SIM_WINDOW_WIDTH, 850));
+		panel.add(this);
+		panel.add(pause_button);
+		panel.add(reset_button);
+
+		frame.add(panel);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
@@ -95,20 +141,18 @@ public class Universe extends Canvas implements ActionListener {
 	public static void main(String[] args) throws InterruptedException {
 		
 		// Create system components
-		ThreeVector pos1 = new ThreeVector(500, 500, 0);
-		ThreeVector pos2 = new ThreeVector(400, 400, 0);
+		ThreeVector pos1 = new ThreeVector(500, 500, 500);
+		ThreeVector pos2 = new ThreeVector(400, 400, 100);
 		ThreeVector vel1 = new ThreeVector(-5, 0, 0);
-		ThreeVector vel2 = new ThreeVector(4, 0, 0);
-
-		Mass[] masses = {new Mass(5, pos1, vel1, 1, Color.RED), new Mass(50000,
-				pos2, vel2, 1, Color.GREEN)};
-		
+		ThreeVector vel2 = new ThreeVector(5, 0, 0);
+		Mass[] masses = {new Mass(5, pos1, vel1, 1, Color.RED), new Mass(5000,
+		pos2, vel2, 1, Color.GREEN)};
 		Universe universe = new Universe(masses);
 		universe.setUpUniverse();
         
 		// Initiate time step
         double time = .0;
-		double deltaTime = .000002;
+		double deltaTime = .00002;
 		
 		// Running boolean
 		boolean running = true;
@@ -128,12 +172,13 @@ public class Universe extends Canvas implements ActionListener {
 			mass2.predict(deltaTime);
 			
 			// Repaint the canvas
-			if ((int) time % 1 == 0) {
+			//if ((int) time % 0.1 == 0) {
 				universe.repaint();
-			}
+			//}
 			
-			time += deltaTime;
-			while (universe.getPaused()) Thread.sleep(100);
+			while (universe.getPaused()) { 
+				Thread.sleep(100);
+			}
 		}
 	}
 
